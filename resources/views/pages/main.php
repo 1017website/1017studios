d4181bea9abbe44262fbf972e6acc3123d3d77a3
zulfi <mochammadzulfi@gmail.com>
2020-02-03 15:10:24 +0700

    update

--- resources/views/pages/main.php
+++ resources/views/pages/main.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <title>1017 STUDIOS</title>
         <link rel='icon' href='/img/logo-navbar.png' type='image/x-icon'/>
-        
+
         <!--SEO-->
         <meta property="og:site_name" content="1017 STUDIOS" />
         <meta property="og:title" content="Index" />
@@ -16,76 +16,85 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="google-site-verification" content="o63PwbeCWP1PAKfmvNvyCNA1JpkeZw3AaFeMREgFrgU" />
         <!--SEO-->
-        
+
         <!--load css-->
         <link href="/css/bootstrap.min.css" rel="stylesheet" type='text/css'>
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type='text/css'>
         <link rel="stylesheet" type="text/css" href="/css/style.css">
         <!--load css-->
-    
+
     </head>
     <body>
-        
+
         <!--navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-black">
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="/img/logo-navbar.png" height="50" alt="1017studios logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ml-auto fs18">
-                    <li class="nav-item pl50">
-                        <a class="nav-link white" href="#">Home</a>
-                    </li>
-                    <li class="nav-item pl50">
-                        <a class="nav-link white" href="#">Profile</a>
-                    </li>
-                    <li class="nav-item pl50">
-                        <a class="nav-link white" href="#">Portofolio</a>
-                    </li>
-                    <li class="nav-item pl50">
-                        <a class="nav-link white" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
+                <a class="navbar-brand" href="#">
+                    <img src="/img/logo-navbar.png" width="auto" height="50" class="d-inline-block align-top" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto fs18">
+                        <a class="nav-item nav-link white" href="#">Home</a>
+                        <a class="nav-item nav-link white" href="#">Profile</a>
+                        <a class="nav-item nav-link white" href="#">Portofolio</a>
+                        <a class="nav-item nav-link white" href="#">Contact</a>
+                    </div>
+                </div>
             </div>
         </nav>
         <!--navbar-->
 
-        <!--home-->
-        <div id="wrapper">
+        <div class="container">
+
+            <!--home-->
             <div id="home">
-                <div class="image-home">
-                    <img src="/img/logo-home.png" height="200" class="logo-home" alt="1017studios logo home">
-                </div>
-                <div class="container center-description">
-                    <div class="row">
-                        <div class="description-home">
-                            We create assets that make your brand stand out. Working with existing brands or helping start-ups,
-                            we work with you to design everything, build website for your product and 3D interior design.
+                <center>
+                    <div id="img-home">
+                        <div class="verticalcenter">
+                            <img src="/img/logo-home.png" height="200" alt="centered image" />
                         </div>
                     </div>
-                </div>
+                    <div class="description-home">
+                        We create assets that make your brand stand out. Working with existing brands or helping start-ups,
+                        we work with you to design everything, build website for your product and 3D interior design.
+                    </div>
+                </center>
             </div>
+            <!--home-->
+
+            <!--profile-->
+            <div id="profile">
+                <center>
+                    <div class="description-profile">
+                        <h1 class="bold">We Create an Assets</h1>
+                        <h5 class="bold">Since 2014</h5>
+                    </div>
+                    <fieldset class="box-profile">
+                        <legend class="w-auto">Your Legend</legend>
+                    </fieldset>
+                </center>
+            </div>
+            <!--profile-->
+
+            <div id="portofolio">
+                <center>
+                    <h1 class="bold">Portofolio</h1>
+                    <h6 class="pt20">Graphic Design</h6>
+                </center>
+            </div>
+
         </div>
-        <!--home-->
-        
-        <!--profile-->
-        <div id="profile">
-            
-        </div>
-        <!--profile-->
-     
+
         <!--social-media-->
         <ul id="social_side_links">
             <li><a class="social-media-button whatsapp" href="#" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
             <li><a class="social-media-button instagram" href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
         </ul>
         <!--social-media-->
-        
+
         <!--load js-->
         <script type="text/javascript" src="/js/jquery.min.js"></script>
         <script type="text/javascript" src="/js/bootstrap.min.js"></script>
