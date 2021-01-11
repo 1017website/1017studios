03d45b6652bb05891a2a6e4e275fc0d64d1b7d62
zulfi <mochammadzulfi@gmail.com>
2021-01-11 09:30:54 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -38,33 +38,32 @@
     <!--/Navbar-->
 
     <!--Intro Section-->
-    <section id="home" class="view" style="background-image: url('https://1017studio.com/1017studios/img/default-background.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <section id="home" class="view" style="background-image: url('https://1017studio.com/1017studios/img/default-background.jpg');">
         <div class="mask">
             <div class="container h-70 d-flex justify-content-center align-items-center">
                 <div class="row pt-5 mt-3">
-                    <div class="col-sm-7 text-center text-md-left" style="margin-top: -2rem;">
+                    <div class="col-sm-7 text-center text-md-left text-header">
                         <div class="white-text">
                             <h4 class="h4-responsive font-weight-bold mt-md-5 mt-0 gotham-narrow-black wow fadeInLeft" data-wow-delay="0.3s">
                                 Mengapa Memilih Kami ?
                             </h4>
-                            <p class="wow fadeInLeft mb-3" data-wow-delay="0.3s" style="font-size: 4rem;font-family: 'GothamNarrow-Black';margin-top: -1.5rem;">
+                            <p class="wow fadeInLeft mb-3 text-header-2" data-wow-delay="0.3s">
                                 KAMI AHLINYA
                             </p>
-                            <p class="wow fadeInLeft mb-3" data-wow-delay="0.3s" style="font-size: 3rem;margin-top: -2rem;color: #ffdd17;font-family: 'Gotham-Medium';line-height: 3rem;">
+                            <p class="wow fadeInLeft mb-3 text-header-3" data-wow-delay="0.3s">
                                 Jasa Desain Logo<br>
                                 dan Website<br>
                                 Sejak 2014
                             </p>
-                            <a href="#" style="font-family: 'Gotham-Light';font-size: 0.8rem; color: white">
+                            <a class="text-header-4" href="#">
                                 <p class="wow fadeInLeft mb-3" data-wow-delay="0.3s" >Baca Selengkapnya ...</p>
                             </a>
-                            <br>
-                            <a href="#" class="btn btn-rounded font-weight-bold ml-lg-0 wow fadeInLeft" data-wow-delay="0.3s" style="font-size: 1.3rem;font-family: 'Gotham-Ultra';width: 20rem;background-color: #ffdd17;color: black;margin-top: 2rem;">
+                            <a href="#" class="btn btn-rounded font-weight-bold ml-lg-0 wow fadeInLeft button-header" data-wow-delay="0.3s">
                                 SAYA MAU PESAN !!
                             </a>
                         </div>
                     </div>
-                    <div class="col-sm-5 text-center text-md-left boat" style="margin-top: 2rem;">
+                    <div class="col-sm-5 text-center text-md-left boat img-header">
                         <img class="img-fluid" src="/1017studios/img/logo.png">
                     </div>
                 </div>
@@ -76,119 +75,119 @@
 <!--/Navigation & Intro-->
 
 <div class="container">
-    <div class="row portofolio-home" id="portofolio-home" style="margin-top: -9rem;">
+    <div class="row portofolio-home" id="portofolio-home">
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-1.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-2.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-3.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-1.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-2.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 1 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 1 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-3.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 2 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 2 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-4.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 2 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 2 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-5.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
             </div>
         </div>
 
-        <div class="col-sm-4 pics animation all 2 view-filter" style="text-align: center;margin-bottom: 30px;">
+        <div class="col-sm-4 pics animation all 2 view-filter portofolio-home-img">
             <div class="waves-effect waves-light">
                 <a href="#!">
                     <div class="view overlay">
                         <img src="/1017studios/img/default-port-6.jpg" class="img-fluid" alt="">
                         <div class="mask flex-center rgba-yellow-strong">
-                            <h4 class="white-text font-weight-bold" style="font-family: 'GothamNarrow-Black';color: black!important;">1017 Studios</h4>
+                            <h4 class="white-text font-weight-bold portofolio-home-text">1017 Studios</h4>
                         </div>
                     </div>
                 </a>
@@ -198,15 +197,15 @@
     </div>
 
     <div class="col-sm-12 d-flex justify-content-center mb-5 pb-2">
-        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter" data-rel="all" style="min-width: 8rem;font-family: 'Gotham-Medium';background-color: #ffdd17!important;">All</button>
-        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter" data-rel="1" style="min-width: 8rem;font-family: 'Gotham-Medium';background-color: #ffdd17!important;">Logo</button>
-        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter" data-rel="2" style="min-width: 8rem;font-family: 'Gotham-Medium';background-color: #ffdd17!important;">Website</button>
+        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="all">All</button>
+        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="1">Logo</button>
+        <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="2">Website</button>
     </div>
 
 </div>
 
 
-<div class="about-home">
+<div class="about-home about-home-img" style="background-image: url('https://1017studio.com/1017studios/img/default-about.jpg');">
 
 </div>
 
