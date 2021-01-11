65816e359cccfd1b2a6d4feef894b26b991d9151
zulfi <mochammadzulfi@gmail.com>
2021-01-11 10:17:34 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -196,7 +196,7 @@
 
     </div>
 
-    <div class="col-sm-12 d-flex justify-content-center mb-5 pb-2">
+    <div class="col-sm-12 d-flex justify-content-center mb2">
         <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="all">All</button>
         <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="1">Logo</button>
         <button type="button" class="btn btn-rounded btn-white font-weight-bold waves-effect filter button-portofolio" data-rel="2">Website</button>
@@ -204,9 +204,33 @@
 
 </div>
 
-
-<div class="about-home about-home-img" style="background-image: url('https://1017studio.com/1017studios/img/default-about.jpg');">
-
+<div class="streak streak-photo streak-md about-home" style="background-image: url('https://1017studio.com/1017studios/img/default-about.jpg');">
+    <div class="container flex-center mask">
+        <div class="text-left white-text">
+            <h3 class="h2-responsive about-home-title">
+                Tentang Kami
+            </h3>
+            <h5 class="h5-responsive about-home-desc">
+                Desain Logo dan Website untuk usaha anda adalah Asset yang sangat berharga.<br><br>
+                1017 Studios di bidang Jasa Desain Logo untuk Brand maupun desain promotion tools seperti desain brosur,
+                desain banner, desain poster dll untuk meningkatkan omzet. 1017 Studios telah berpengalaman berdiri sejak
+                2014 memiliki visi misi meningkatkan daya jual pemasaran melalui desain yang berkualitas.<br><br>
+                Desain Logo adalah identitas dari sebuah produk, sehingga harus di desain dengan serius sesuai dengan target
+                marketnya. Selain desain logo, kami berpengalaman dalam desain grafis lainnya seperti brosur, office tools,
+                packaging, dan lainnya. Perkembangan Desain Logo saat ini tidak hanya digunakan di perusahaan besar saja,
+                namun di terapkan pada pengusaha UMKM, Start Up, dan lainnya, hal itu dikarenakan karena Desain Logo
+                sangat membantu dalam menyampaikan brand image.<br><br>
+                Desain logo keren memiliki fungsi yang cukup penting bagi sebuah perusahaan maupun brand. Dimana logo
+                termasuk dalam program marketing yang berfungsi sebagai brand image. Desain logo sangat berperan dalam
+                perkembangan brand, program marketing, identitas dan lainnya. Desain Logo yang baik adalah dapat
+                menimbulkan citra / image dari sebuah brand sesuai dari visi dan misi brand.<br><br>
+                Desain logo / desain grafis yang kami hasilkan adalah desain yang original, sehingga tidak usah khawatir
+                bahwa desain logo / desain grafis anda sama dengan lainnya. Kami berpengalaman lebih dari 5 tahun dalam
+                pembuatan desain logo, desain grafis, web development, 3d desain yang tentunya sesuai dengan kebutuhan
+                pasar.
+            </h5>
+        </div>
+    </div>
 </div>
 
 @endsection
