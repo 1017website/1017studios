ebf24571249e7c645acb0d92310298fa1eb886a7
SoffiaNoor <soffianaisyah0@gmail.com>
2023-08-01 09:18:21 +0700

    *test

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -27,9 +27,67 @@
                 </h2>
             </div>
             <div class="grid items-center gap-4 xl:grid-cols-2">
-                <div class="mt-4 xl:mt-5 space-y-6 px-4 lg:px-4" data-aos="zoom-out-up" data-aos-duration="1000">
-                    <p class="font-semibold text-white sm:text-xl">Tinggalkan pesan disini</p>
-                    <form action="#" class="space-y-8">
+                <form class="w-full max-w-lg">
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-first-name">
+                                First Name
+                            </label>
+                            <input
+                                class="appearance-none block w-full input input-bordered input-warning"
+                                id="grid-first-name" type="text" placeholder="Jane">
+                            <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-last-name">
+                                Last Name
+                            </label>
+                            <input
+                                class="appearance-none block w-full input input-bordered input-warning"
+                                id="grid-last-name" type="text" placeholder="Doe">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-password">
+                                E-mail
+                            </label>
+                            <input
+                                class="appearance-none block w-full input input-bordered input-warning"
+                                id="email" type="email">
+                            <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-password">
+                                Message
+                            </label>
+                            <textarea
+                                class=" no-resize appearance-none block w-full textarea textarea-warning resize-none"
+                                id="message"></textarea>
+                            <p class="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none /
+                                resize-y / resize-x / resize</p>
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center">
+                        <div class="md:w-1/3">
+                            <button
+                                class="btn btn-warning bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500"
+                                type="button">
+                                Send
+                            </button>
+                        </div>
+                        <div class="md:w-2/3"></div>
+                    </div>
+                </form>
+                {{-- <div class="mt-4 xl:mt-5 space-y-6 px-4 lg:px-4" data-aos="zoom-out-up" data-aos-duration="1000">
+                    <p class="font-semibold text-white sm:text-xl">Tinggalkan pesan disini</p> --}}
+                    {{-- <form action="#" class="space-y-8">
                         <div class="flex flex-col md:flex-row">
                             <div class="md:mr-2 mb-2 md:mb-0 w-full">
                                 <label for="first_name"
@@ -70,12 +128,40 @@
                         </div>
                         <button
                             class="btn btn-warning w-full bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500">SUBMIT</button>
-                    </form>
-                </div>
+                    </form> --}}
+                    {{--
+                </div> --}}
                 <div class="p-6 xl:col-span-1">
-                    <div class="grid gap-4 md:grid-cols-2">
+                    <div class="grid gap-4 md:grid-cols-1">
                         <div class="grid content-center gap-4">
-                            <div class="p-6 rounded shadow-md bg-[#FFE200] text-black font-semibold"
+                            <div class="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-1 xl:text-left"
+                                data-aos="zoom-out-up" data-aos-duration="1000">
+                                <h2 class="lg:text-2xl text-2xl text-white font-extrabold sm:pr-8 xl:pr-12">
+                                    Hubungi kami sekarang</h2>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fab fa-whatsapp text-white text-2xl mr-2" aria-hidden="true"></i>081 938
+                                    64 65 60</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fab fa-instagram text-white text-2xl mr-2"
+                                        aria-hidden="true"></i>studiodesign1017</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fa fa-envelope text-white text-2xl mr-2"
+                                        aria-hidden="true"></i>mahendrataditya@gmail.com</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fa fa-caret-down text-white text-2xl mr-2" aria-hidden="true"></i>Klik
+                                    button di bawah ini untuk konsultasi desain</p>
+                                <button
+                                    class="btn btn-warning w-full bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500">Konsultasi
+                                    Desain
+                                </button>
+                                <div class="map-container">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7914.8693808254975!2d112.782346!3d-7.304963000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7faf5f5654195%3A0x240bfb7744077396!2sJunior%20High%20School%20State%2052%20of%20SURABAYA!5e0!3m2!1sen!2sid!4v1690790159441!5m2!1sen!2sid"
+                                        width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                            {{-- <div class="p-6 rounded shadow-md bg-[#FFE200] text-black font-semibold"
                                 data-aos="zoom-out-up" data-aos-duration="1000">
                                 <p>Tks sdh bantu pembuatan logo yayasan tk kami.. di luar dugaan saya, ternyata hasilnya
                                     sangat luar biasa... bagus👍... dengan logo baru ini tk kami lebih d minati... tks
@@ -163,7 +249,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
@@ -217,7 +303,7 @@
                 <div>
                     <div class="grid gap-4 md:grid-cols-1">
                         <div class="grid gap-4">
-                            <div class="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left"
+                            <div class="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-1 xl:text-left"
                                 data-aos="zoom-out-up" data-aos-duration="1000">
                                 <h2 class="lg:text-2xl text-2xl text-white font-extrabold sm:pr-8 xl:pr-12">
                                     Hubungi kami sekarang</h2>
@@ -240,7 +326,7 @@
                                 <div class="map-container">
                                     <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7914.8693808254975!2d112.782346!3d-7.304963000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7faf5f5654195%3A0x240bfb7744077396!2sJunior%20High%20School%20State%2052%20of%20SURABAYA!5e0!3m2!1sen!2sid!4v1690790159441!5m2!1sen!2sid"
-                                        width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                                        width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"
                                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </div>
