c91cb89b60260b81c4db63e5dae1891e4a0ce0dc
SoffiaNoor <soffianaisyah0@gmail.com>
2023-08-01 08:26:35 +0700

    *update kontak

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -26,11 +26,86 @@
                     Kontak
                 </h2>
             </div>
-            <div class="grid grid-cols-4 gap-4">
-                <div>01</div>
-                <!-- ... -->
-                <div>09</div>
-              </div>
+            <div class="grid xl:grid-cols-2 gap-4">
+                <div class="mt-4 xl:mt-5 space-y-6 px-4 lg:px-4" data-aos="zoom-out-up" data-aos-duration="1000">
+                    <p class="font-semibold text-white sm:text-xl">Tinggalkan pesan disini</p>
+                    <form action="#" class="space-y-8">
+                        <div class="flex flex-col md:flex-row">
+                            <div class="md:mr-2 mb-2 md:mb-0 w-full">
+                                <label for="first_name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First
+                                    Name</label>
+                                <input type="name" id="first_name"
+                                    class="input input-bordered input-warning w-full max-w-xs" placeholder="first"
+                                    required />
+                            </div>
+                            <div class="md:ml-2 w-full">
+                                <label for="last_name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last
+                                    Name</label>
+                                <input type="name" id="last_name"
+                                    class="input input-bordered input-warning w-full max-w-xs" placeholder="last"
+                                    required />
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <label for="email"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                            <input type="email" id="email" class="input input-bordered input-warning w-full max-w-xl"
+                                placeholder="example@gmail.com" required>
+                        </div>
+                        <div class="w-full">
+                            <label for="number"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nomor
+                                Telepon/Whatsapp</label>
+                            <input type="telephone" id="telephone"
+                                class="input input-bordered input-warning w-full max-w-xl" placeholder="+62 000 000 000"
+                                required>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="message"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Pesan</label>
+                            <textarea class="textarea textarea-warning w-full"
+                                placeholder="Tinggalkan pesan disini"></textarea>
+                        </div>
+                        <button
+                            class="btn btn-warning w-full bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500">SUBMIT</button>
+                    </form>
+                </div>
+                <div>
+                    <div class="grid gap-4 md:grid-cols-1">
+                        <div class="grid gap-4">
+                            <div class="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left"
+                                data-aos="zoom-out-up" data-aos-duration="1000">
+                                <h2 class="lg:text-2xl text-2xl text-white font-extrabold sm:pr-8 xl:pr-12">
+                                    Hubungi kami sekarang</h2>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fab fa-whatsapp text-white text-2xl mr-2" aria-hidden="true"></i>081 938
+                                    64 65 60</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fab fa-instagram text-white text-2xl mr-2"
+                                        aria-hidden="true"></i>studiodesign1017</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fa fa-envelope text-white text-2xl mr-2"
+                                        aria-hidden="true"></i>mahendrataditya@gmail.com</p>
+                                <p class="mb-8 lg:mb-16 font-semibold text-white sm:text-xl"><i
+                                        class="fa fa-caret-down text-white text-2xl mr-2" aria-hidden="true"></i>Klik
+                                    button di bawah ini untuk konsultasi desain</p>
+                                <button
+                                    class="btn btn-warning w-full bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500">Konsultasi
+                                    Desain
+                                </button>
+                                <div class="map-container">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7914.8693808254975!2d112.782346!3d-7.304963000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7faf5f5654195%3A0x240bfb7744077396!2sJunior%20High%20School%20State%2052%20of%20SURABAYA!5e0!3m2!1sen!2sid!4v1690790159441!5m2!1sen!2sid"
+                                        width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
