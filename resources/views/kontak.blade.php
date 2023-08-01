f301b63160ae66be93421ba15a2889ca1c80a7c8
SoffiaNoor <soffianaisyah0@gmail.com>
2023-08-01 09:04:44 +0700

    Update kontak.blade.php

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -26,7 +26,7 @@
                     Kontak
                 </h2>
             </div>
-            <div class="grid items-center gap-4 xl:grid-cols-5">
+            <div class="grid items-center gap-4 xl:grid-cols-2">
                 <div class="mt-4 xl:mt-5 space-y-6 px-4 lg:px-4" data-aos="zoom-out-up" data-aos-duration="1000">
                     <p class="font-semibold text-white sm:text-xl">Tinggalkan pesan disini</p>
                     <form action="#" class="space-y-8">
@@ -72,22 +72,7 @@
                             class="btn btn-warning w-full bg-[#FFE200] font-extrabold text-black hover:bg-[#c79305] border-none duration-500">SUBMIT</button>
                     </form>
                 </div>
-                {{-- <div class="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left"
-                    data-aos="zoom-out-up" data-aos-duration="1000">
-                    <h2 class="lg:text-5xl text-2xl text-[#FFE200] font-extrabold sm:pr-8 xl:pr-12">Testimoni</h2>
-                    <p class="paragraph text-white lg:text-xl text-justify ">Impresi mereka terhadap kami</p>
-                    <div
-                        class="flex flex-col sm:flex-row space-y-6 sm:space-y-0 place-content-center sm:space-x-6 pt-3 mt-2 aos-init aos-animate">
-                        <blockquote data-aos="zoom-out-up" data-aos-duration="1000"
-                            class="text-2xl font-semibold italic text-center text-white">
-                            <a href="#"
-                                class="before:block before:absolute before:-inset-2 before:-skew-y-3 before:bg-[#FFE200] relative inline-block">
-                                <span class="relative text-black font-extrabold">Baca Selengkapnya..</span>
-                            </a>
-                        </blockquote>
-                    </div>
-                </div> --}}
-                <div class="p-6 xl:col-span-3">
+                <div class="p-6 xl:col-span-1">
                     <div class="grid gap-4 md:grid-cols-2">
                         <div class="grid content-center gap-4">
                             <div class="p-6 rounded shadow-md bg-[#FFE200] text-black font-semibold"
@@ -144,97 +129,6 @@
                                         class="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500">
                                     <div>
                                         <p class="text-lg font-semibold">Rizki Fajrillah</p>
-                                        <div class="flex items-center space-x-1">
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="grid content-center gap-4">
-                            <div class="p-6 rounded shadow-md bg-[#FFE200] text-black font-semibold"
-                                data-aos="zoom-out-up" data-aos-duration="1000">
-                                <p>Recomended banget, fast respon dan hasilnya top markotop.. gak nyesel deh order
-                                    disini sesuai budget banget...</p>
-                                <div class="flex items-center mt-4 space-x-4">
-                                    <img src="{{ asset('assets/img/testimoni.png') }}" alt=""
-                                        class="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500">
-                                    <div>
-                                        <p class="text-lg font-semibold">Santa Aldara</p>
-                                        <div class="flex items-center space-x-1">
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                            <svg class="w-4 h-4 text-black-300" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                viewBox="0 0 22 20">
-                                                <path
-                                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-6 rounded shadow-md bg-[#FFE200] text-black font-semibold"
-                                data-aos="zoom-out-up" data-aos-duration="1000">
-                                <p>Terima kasih banyak sdh membuatkan logo kami. Kami sangat puas, fast respon, sabar dg
-                                    aku yg super duper cerewet😄, hasilnya logo jg elegan sesuai dg yg kami inginkan.
-                                    Murah tp tdk murahan😉 Sukses selalu ya....👍 </p>
-                                <div class="flex items-center mt-4 space-x-4">
-                                    <img src="{{ asset('assets/img/testimoni.png') }}" alt=""
-                                        class="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500">
-                                    <div>
-                                        <p class="text-lg font-semibold">Berdiyani Eliyanti</p>
                                         <div class="flex items-center space-x-1">
                                             <svg class="w-4 h-4 text-black-300" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor"
